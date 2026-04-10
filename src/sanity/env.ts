ae047dfa38c65ddac8d1888fbfbda95aac4b0c10
benajax <benojong@hotmail.com>
2026-04-10 17:04:56 +0100

    Studio host

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,15 +1,16 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-26'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-15'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'development', // Fallback value
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'q31y36ab', // Fallback value
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
+
+export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
